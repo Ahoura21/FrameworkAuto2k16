e5cdf5cc94debec3664a13dc38b0bd0082c4bb83
Ahoura <nirvana.atashin@gmail.com>
2017-01-12 14:37:30 -0500

    framework

---
+++ Amazon/src/test/java/page/TestHomeAmazon.java
@@ -0,0 +1,17 @@
+package page;
+
+import base.CommonAPI;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+/**
+ * Created by ahoura on 1/10/17.
+ */
+public class TestHomeAmazon extends CommonAPI {
+    @Test
+    public void TestHome(){
+        //System.out.println(driver.getCurrentUrl());
+       // driver.findElement(By.xpath("//*[@id='nav-xshop']/a[2]")).click();
+    }
+
+}
---
+++ Cnn/src/test/java/cnnHome/TestCnn.java
@@ -0,0 +1,16 @@
+package cnnHome;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+
+/**
+ * Created by ahoura on 1/10/17.
+ */
+public class TestCnn extends CommonAPI {
+@Test
+public void testCnn(){
+   // System.out.println(driver.getCurrentUrl());
+}
+
+
+}
